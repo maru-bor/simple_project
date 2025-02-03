35cb372d81189ce6b8aefa879d77d5e44f714e78
borisova <borisova.marie1@gmail.com>
2025-02-03 13:15:38 +0100

    Added file Temp.tsx

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -4,20 +4,17 @@ import {useState} from "react";
 
 
 function Counter(){
-    const[count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-    return (
-        <>
-            <button onClick={() => setCount((count) => count + 1)}> Add 1 </button>
+  return (
+    <>
+      <button onClick={() => setCount(count => count + 1)}> Add 1</button>
 
-            <button onClick={() => setCount((count) => count - 1)}> Subtract 1</button>
+      <button onClick={() => setCount(count => count - 1)}> Subtract 1</button>
 
-            <div> count is {count}</div>
-        </>
-
-
-    )
-
+      <div> count is {count}</div>
+    </>
+  );
 }
 
 export default Counter
