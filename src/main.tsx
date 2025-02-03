5f183dbf484785a246941820325291f3dbb9e5e9
borisova <borisova.marie1@gmail.com>
2025-02-03 14:18:24 +0100

    Added file AgeCalculator.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import Counter from "./Counter.tsx";
-import BgColor from "./BgColor.tsx";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import Counter from './Counter.tsx';
+import BgColor from './BgColor.tsx';
+import AgeCalculator from './AgeCalculator.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-      <Counter/>
-      <Counter/>
-      <BgColor/>
+    <Counter />
+    <Counter />
+    <AgeCalculator />
+    <BgColor />
   </StrictMode>,
-)
+);
